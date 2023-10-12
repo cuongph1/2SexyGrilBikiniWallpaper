6e09d4499da2cadc79a0c6eede4281c9e82477fa
hc88media <hc88media@yahoo.com>
2023-10-12 21:08:21 +0700

    f

---
+++ command.cmd
@@ -0,0 +1,4 @@
+@rem param1 duong dan anh
+@rem param2 dung dan folder github
+C:\openjdk-17.0.2\bin\java.exe -jar cuong.jar E:\Githubs\17kuongaixinh\2SexyGrilBikiniWallpaper\anh E:\Githubs\17kuongaixinh\2SexyGrilBikiniWallpaper\2SexyGrilBikiniWallpaper
+pause
